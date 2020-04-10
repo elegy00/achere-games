7195fdcbec31dffb36d33c76d42b358ee4e4c8c5
Christoph <losspammos@gmail.com>
2020-04-10 23:19:15 +0200

    Add React-Router

---
+++ src/layout/BaseLayout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+interface Props {
+  children: React.ReactChild;
+}
+
+const BaseLayout: React.FC<Props> = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <header>
+        <Link to="/login">Login</Link>
+        <Link to="/games">Games</Link>
+      </header>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default BaseLayout;
